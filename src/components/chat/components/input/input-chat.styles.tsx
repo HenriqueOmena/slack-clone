1e8d8e7ff4b9236c8bf33e9911057fbcd96aeeb2
HenriqueOmena <88lmao@gmail.com>
2020-09-20 23:39:54 +0100

    Wp: chat styled to fix on bottom

--- src/components/chat/components/input/input-chat.styles.tsx
+++ src/components/chat/components/input/input-chat.styles.tsx
@@ -1,9 +1,26 @@
 import { makeStyles, createStyles } from '@material-ui/core';
+import { relative } from 'path';
 
 const inputStyle = makeStyles(theme =>
   createStyles({
-    chatInput: {
-      border: 'solid 1px red',
+    container: {
+      borderRadius: 20,
+    },
+    chatForm: {
+      position: 'relative',
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    inputText: {
+      position: 'fixed',
+      bottom: 30,
+      width: '60%',
+      border: 'solid 1px gray',
+      borderRadius: 3,
+      padding: 20,
+    },
+    displayNone: {
+      display: 'none',
     },
   })
 );
